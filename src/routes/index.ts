824c1475d3a23c46e2f665bb1c9a4489a268416a
Denis Slapelis <denis.slapelis.dev@gmail.com>
2024-03-16 19:53:48 -0300

    Add nosniff header

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,20 +1,21 @@
 import { healthCheckController, loginController } from '../utils/dependency.utils';
 import { Router } from 'express';
-import { router as routesV1 } from './v1/routes'
+import { router as routesV1 } from './v1/routes';
 
 export const router = Router();
 
-// health check
-const healthCheckRoutes = Router();
-healthCheckRoutes.get('/', healthCheckController.check);
+const noSniff = (req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    next();
+};
 
 // login
 const loginRoutes = Router();
-loginRoutes.post('/', loginController.login);
+loginRoutes.post('/', noSniff, loginController.login);
 
 // custom routes
-router.use('/healthcheck', healthCheckRoutes);
-router.use('/login', loginRoutes);
+router.use('/healthcheck', noSniff, Router().get('/', healthCheckController.check));
+router.use('/login', noSniff, loginRoutes);
 
 // v1 routes
-router.use('/api/v1', routesV1);
+router.use('/api/v1', noSniff, routesV1);
